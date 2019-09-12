2a5a32d23cb407a38d803fc22545dfed347ebd66
chris-bizness <chris.thatcher.dev@gmail.com>
2019-09-12 04:05:11 -0700

    Set up auto-schema documentation

--- project/urls.py
+++ project/urls.py
@@ -1,5 +1,22 @@
 from django.urls import path, include
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
-    path('', include('card_api.urls')),
+    path('card-number/', include('card_api.urls')),
+
+    # Use the `get_schema_view()` helper to add a `SchemaView` to project URLs.
+    #   * `title` and `description` parameters are passed to `SchemaGenerator`.
+    #   * Provide view name for use with `reverse()`.
+    path('openapi', get_schema_view(
+        title="Albert Case Study",
+        description="API for validating and generating payment card numbers",
+    ), name='openapi-schema'),
+
+    # Route TemplateView to serve Swagger UI template.
+    #   * Provide `extra_context` with view name of `SchemaView`.
+    path('', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
 ]
