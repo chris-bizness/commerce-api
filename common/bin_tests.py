95ba48a200feaf484ff91002f0e9344030d402b3
chris-bizness <chris.thatcher.dev@gmail.com>
2019-09-12 04:00:57 -0700

    Add BIN tests for major issuers

---
+++ common/bin_tests.py
@@ -0,0 +1,30 @@
+"""
+    All BIN range values found @ https://www.bincodes.com/bin-list/
+"""
+
+
+def is_visa(card_number: str) -> bool:
+    return card_number.startswith("4")
+
+
+def is_master_card(card_number: str) -> bool:
+    return (
+        "2221" <= card_number[:4] <= "2720"
+        or "51" <= card_number[:2] <= "55"
+    )
+
+
+def is_discover_card(card_number: str) -> bool:
+    return (
+        card_number.startswith("6011")
+        or card_number.startswith("65")
+        or "622126" <= card_number[:6] <= "622925"
+        or "644" <= card_number[:3] <= "649"
+    )
+
+
+def is_american_express(card_number: str) -> bool:
+    return (
+        card_number.startswith("34")
+        or card_number.startswith("37")
+    )
